23e380d8a194ad00747cfcbe285c8e3f7a8eae19
Leticia Monteiro <leticiamonteiro.design@gmail.com>
2021-06-28 18:06:42 -0300

    chore: initial commit project

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '04fbb47693badef432e679b9136552c1'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4a3a1c82-0632-4a80-9730-ce8c8fc253d8'
+};
