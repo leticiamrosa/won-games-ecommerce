f10dbb07844fca55270e7a47627f56840fae1071
leticiamrosa <leticiamonteiro.design@gmail.com>
2021-08-22 20:54:07 -0300

    feat: apply customizate on the hover at left menu links

---
+++ api/admin/src/components/LeftMenu/LeftMenuLink/A.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const A = styled.a`
+  position: relative;
+  padding-top: 0.7rem;
+  padding-bottom: 0.2rem;
+  padding-left: 1.6rem;
+  min-height: 3.6rem;
+  border-left: 0.3rem solid transparent;
+  cursor: pointer;
+  color: ${({ theme }) => theme.main.colors.leftMenu["link-color"]};
+  text-decoration: none;
+  display: block;
+  -webkit-font-smoothing: antialiased;
+  &:hover {
+    color: ${({ theme }) => theme.main.colors.white};
+    background: ${({ theme }) => theme.main.colors.won.blue};
+    border-left: 0.3rem solid ${({ theme }) => theme.main.colors.won.orange};
+    text-decoration: none;
+  }
+  &:focus {
+    color: ${({ theme }) => theme.main.colors.white};
+    text-decoration: none;
+  }
+  &:visited {
+    color: ${({ theme }) => theme.main.colors.leftMenu["link-color"]};
+  }
+  &.linkActive {
+    color: white !important;
+    border-left: 0.3rem solid ${({ theme }) => theme.main.colors.won.orange};
+  }
+`;
+
+export default A;
