87da78b6bdfac190a60a11a921d00f4bf71a8d57
leticiamrosa <leticiamonteiro.design@gmail.com>
2021-08-22 22:39:13 -0300

    feat: implementation the structure data

--- api/api/games/controllers/games.js
+++ api/api/games/controllers/games.js
@@ -1,8 +1,17 @@
 'use strict';
 
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+ module.exports = {
+    populate: async (ctx) => {
+      console.log("Starting to populate");
+
+      await strapi.services.games.populate();
+
+      ctx.send({ status: 200 });
+    }
+  };
